a6bfaaed81a2920ae6ea4c0177fe40150f52db3d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-01 20:13:51 +0545

    updated language code and staticfile

--- credit_information_system/settings.py
+++ credit_information_system/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_FILES_DIRS = [BASE_DIR / 'static']
