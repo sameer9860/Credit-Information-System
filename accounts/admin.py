fb0b8d4ab1438f966fadb9c4d6a8fc4cf24f3ae1
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-02 18:37:00 +0545

    Added user model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Custom User admin with role and cooperative fields."""
+    
+    list_display = ('username', 'email', 'get_role_display', 'cooperative', 'is_staff', 'is_active')
+    list_filter = ('role', 'cooperative', 'is_active', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('role', 'cooperative')
+        }),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('role', 'cooperative')
+        }),
+    )
+    ordering = ('-date_joined',)
+
