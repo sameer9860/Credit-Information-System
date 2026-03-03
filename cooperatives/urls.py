3f9b8503f93cdbe788217c5498af0104558702f7
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-03 18:49:59 +0545

    Created urls for cops

---
+++ cooperatives/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'cooperatives'
+
+urlpatterns = [
+    path('', views.CooperativeListView.as_view(), name='list'),
+    path('add/', views.CooperativeCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.CooperativeDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.CooperativeUpdateView.as_view(), name='edit'),
+    path('<int:pk>/toggle-status/', views.toggle_cooperative_status, name='toggle_status'),
+]
