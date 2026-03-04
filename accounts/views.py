9b6864fe694cc604a52a1611151b17763ceade09
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-04 15:52:39 +0545

    Added dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from cooperatives.models import Cooperative
+from members.models import Member
+from django.db.models import Count
 
-# Create your views here.
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    """Main dashboard view for all users."""
+    template_name = 'accounts/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_superadmin():
+            # System-wide stats for Super Admin
+            context['total_cooperatives'] = Cooperative.objects.count()
+            context['active_cooperatives'] = Cooperative.objects.filter(status='active').count()
+            context['total_members'] = Member.objects.count()
+            context['blacklisted_members'] = Member.objects.filter(blacklist_status=True).count()
+            context['recent_members'] = Member.objects.all().order_by('-created_at')[:5]
+            context['recent_cooperatives'] = Cooperative.objects.all().order_by('-created_at')[:5]
+        else:
+            # Stats for Cooperative Admin/Staff
+            if user.cooperative:
+                coop = user.cooperative
+                context['total_members'] = Member.objects.filter(cooperative=coop).count()
+                context['blacklisted_members'] = Member.objects.filter(cooperative=coop, blacklist_status=True).count()
+                context['recent_members'] = Member.objects.filter(cooperative=coop).order_by('-created_at')[:5]
+                context['my_cooperative'] = coop
+
+        return context
