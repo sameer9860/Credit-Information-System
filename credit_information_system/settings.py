e8982dea5df9f2aeee9d3077738011228149532d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-04 15:53:36 +0545

    Added login logout urls

--- credit_information_system/settings.py
+++ credit_information_system/settings.py
@@ -111,6 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+# Authentication URLs
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'accounts:dashboard'
+LOGOUT_REDIRECT_URL = 'accounts:login'
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
