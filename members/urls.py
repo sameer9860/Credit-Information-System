1abf3cb517e95e0c754e2f83e03a53934339d056
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-04 15:14:57 +0545

    Created:members url

---
+++ members/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'members'
+
+urlpatterns = [
+    path('', views.MemberListView.as_view(), name='member_list'),
+    path('add/', views.MemberCreateView.as_view(), name='member_add'),
+    path('<int:pk>/', views.MemberDetailView.as_view(), name='member_detail'),
+    path('<int:pk>/edit/', views.MemberUpdateView.as_view(), name='member_edit'),
+    path('<int:pk>/delete/', views.MemberDeleteView.as_view(), name='member_delete'),
+]
