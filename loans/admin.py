5064743633d93e217f68f0344afc757218ef7c0a
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-05 15:05:04 +0545

    Added loan model

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Loan
 
-# Register your models here.
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "loan_id",
+        "member",
+        "cooperative",
+        "loan_amount",
+        "remaining_balance",
+        "status",
+        "loan_date",
+        "due_date"
+    )
+
+    list_filter = (
+        "status",
+        "cooperative",
+        "loan_date"
+    )
+
+    search_fields = (
+        "member__name",
+        "loan_id"
+    )
