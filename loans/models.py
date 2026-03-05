d1ef17df17c71ff864b0c3c762def1ab5b49e3a0
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-05 15:04:32 +0545

    Removed warnings

--- loans/models.py
+++ loans/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from members.models import Member
 from cooperatives.models import Cooperative
