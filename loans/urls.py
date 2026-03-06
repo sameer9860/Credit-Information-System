8610be6bfa463df48d6bdef5e9c0c216c9d972d5
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-06 19:30:13 +0545

    Added gurantor url

--- loans/urls.py
+++ loans/urls.py
@@ -2,15 +2,24 @@ from django.urls import path
 from .views import (
     LoanListView, LoanDetailView,
     LoanCreateView, LoanUpdateView,
-    LoanDeleteView
+    LoanDeleteView,
+    GuarantorListView, GuarantorCreateView,
+    GuarantorUpdateView, GuarantorDeleteView
 )
 
 app_name = 'loans'  # important for namespacing
 
 urlpatterns = [
+    # Loan URLs
     path('', LoanListView.as_view(), name='loan_list'),
     path('create/', LoanCreateView.as_view(), name='create_loan'),
     path('<int:loan_id>/', LoanDetailView.as_view(), name='loan_detail'),
     path('<int:loan_id>/update/', LoanUpdateView.as_view(), name='update_loan'),
     path('<int:loan_id>/delete/', LoanDeleteView.as_view(), name='delete_loan'),
+
+    # Guarantor URLs
+    path('guarantors/', GuarantorListView.as_view(), name='guarantor_list'),
+    path('guarantors/add/', GuarantorCreateView.as_view(), name='add_guarantor'),
+    path('guarantors/<int:pk>/update/', GuarantorUpdateView.as_view(), name='update_guarantor'),
+    path('guarantors/<int:pk>/delete/', GuarantorDeleteView.as_view(), name='delete_guarantor'),
 ]
