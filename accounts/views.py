2c30636c48165ab57806a0ee15ad0b926eabe5b4
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-08 18:26:11 +0545

    Added guarantors context

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from cooperatives.models import Cooperative
 from members.models import Member
-from loans.models import Loan
+from loans.models import Loan, Guarantor
 from django.db.models import Count
 
 
@@ -20,7 +20,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
             context['active_cooperatives'] = Cooperative.objects.filter(status='active').count()
             context['total_members'] = Member.objects.count()
             context['blacklisted_members'] = Member.objects.filter(blacklist_status=True).count()
-            
+            context['total_guarantors'] = Guarantor.objects.count()
             # Phase 10: Loan stats (Super Admin)
             context['total_loans'] = Loan.objects.count()
             context['system_overdue_loans'] = Loan.objects.filter(status='Overdue').count()
