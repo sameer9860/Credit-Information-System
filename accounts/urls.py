ad40589f4dce465ca55293c7806339ff0fb9b64c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-09 19:52:55 +0545

    Added : Change Passeord first time

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
 ]
