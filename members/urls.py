a4aa54cfd946e99156a79b7ffff442f773388d47
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-09 19:47:29 +0545

    feat add import and export

--- members/urls.py
+++ members/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('<int:pk>/', views.MemberDetailView.as_view(), name='member_detail'),
     path('<int:pk>/edit/', views.MemberUpdateView.as_view(), name='member_edit'),
     path('<int:pk>/delete/', views.MemberDeleteView.as_view(), name='member_delete'),
+    # CSV import / export
+    path('export/csv/', views.MemberExportCSVView.as_view(), name='member_export_csv'),
+    path('import/csv/', views.MemberImportCSVView.as_view(), name='member_import_csv'),
+    path('import/csv/template/', views.MemberDownloadTemplatCSVView.as_view(), name='member_csv_template'),
 ]
